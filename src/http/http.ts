a9a8accf4c2799cee373aab9081eb8d1adf30e51
Daniil <daniil.karpenk0@yandex.ru>
2025-02-17 00:16:43 +0500

    changed api url

--- src/http/http.ts
+++ src/http/http.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const http = axios.create({
-    baseURL: 'https://amvera-wannasleep66-run-telegram-events/api',
+    baseURL: 'https://telegram-events-wannasleep66.amvera.io/api',
 })
